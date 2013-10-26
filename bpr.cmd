2a3add34647261b8e1410531cdb0c1c773b61550
uncleF <uncle.funkay@gmail.com>
2013-10-26 21:39:47 +0400

    Project Changes

--- bpr.cmd
+++ bpr.cmd
@@ -1,10 +1,15 @@
 @ECHO OFF
 
-shareFlag=false
+set shareFlag="false"
 IF NOT "%1"=="" (
 	IF "%1"=="-s" (
 		shareFlag=true
 	) ELSE (
 		echo "Invalid Parameters"
 	)
+)
+IF %shareFlag%=="true" (
+	ant -propertyfile %buildProperties% -Dbuild.share=share
+) ELSE (
+	ant -propertyfile %buildProperties%
 )
