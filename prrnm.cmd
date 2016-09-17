8d66e62912f1c1ca99ce178d6bcd6e4252794d4e
uncleF <uncle.funkay@gmail.com>
2016-09-17 21:23:58 +0300

    Project Update

--- prrnm.cmd
+++ prrnm.cmd
@@ -6,7 +6,7 @@ set newProjectName=
 
 :options
 	if [%1]==[] (
-		goto projectSetup
+		goto projectRename
 	) else (
 		set newProjectName=%1
 	)
@@ -25,71 +25,70 @@ set newProjectName=
 	if [%newProjectName%]==[] (
 		goto errorName
 	)
-	if exist dev (
-		set oldProjectName=%cd%
-		echo Renaming Project %oldProjectName%
-		set oldPackageName=%oldProjectName%
-		set oldPackageName=%oldPackageName:A=a%
-		set oldPackageName=%oldPackageName:B=b%
-		set oldPackageName=%oldPackageName:C=c%
-		set oldPackageName=%oldPackageName:D=d%
-		set oldPackageName=%oldPackageName:E=e%
-		set oldPackageName=%oldPackageName:F=f%
-		set oldPackageName=%oldPackageName:G=g%
-		set oldPackageName=%oldPackageName:H=h%
-		set oldPackageName=%oldPackageName:I=i%
-		set oldPackageName=%oldPackageName:J=j%
-		set oldPackageName=%oldPackageName:K=k%
-		set oldPackageName=%oldPackageName:L=l%
-		set oldPackageName=%oldPackageName:M=m%
-		set oldPackageName=%oldPackageName:N=n%
-		set oldPackageName=%oldPackageName:O=o%
-		set oldPackageName=%oldPackageName:P=p%
-		set oldPackageName=%oldPackageName:Q=q%
-		set oldPackageName=%oldPackageName:R=r%
-		set oldPackageName=%oldPackageName:S=s%
-		set oldPackageName=%oldPackageName:T=t%
-		set oldPackageName=%oldPackageName:U=u%
-		set oldPackageName=%oldPackageName:V=v%
-		set oldPackageName=%oldPackageName:W=w%
-		set oldPackageName=%oldPackageName:X=x%
-		set oldPackageName=%oldPackageName:Y=y%
-		set oldPackageName=%oldPackageName:Z=z%
-		set newPackageName=%newProjectName%
-		set newPackageName=%newPackageName:A=a%
-		set newPackageName=%newPackageName:B=b%
-		set newPackageName=%newPackageName:C=c%
-		set newPackageName=%newPackageName:D=d%
-		set newPackageName=%newPackageName:E=e%
-		set newPackageName=%newPackageName:F=f%
-		set newPackageName=%newPackageName:G=g%
-		set newPackageName=%newPackageName:H=h%
-		set newPackageName=%newPackageName:I=i%
-		set newPackageName=%newPackageName:J=j%
-		set newPackageName=%newPackageName:K=k%
-		set newPackageName=%newPackageName:L=l%
-		set newPackageName=%newPackageName:M=m%
-		set newPackageName=%newPackageName:N=n%
-		set newPackageName=%newPackageName:O=o%
-		set newPackageName=%newPackageName:P=p%
-		set newPackageName=%newPackageName:Q=q%
-		set newPackageName=%newPackageName:R=r%
-		set newPackageName=%newPackageName:S=s%
-		set newPackageName=%newPackageName:T=t%
-		set newPackageName=%newPackageName:U=u%
-		set newPackageName=%newPackageName:V=v%
-		set newPackageName=%newPackageName:W=w%
-		set newPackageName=%newPackageName:X=x%
-		set newPackageName=%newPackageName:Y=y%
-		set newPackageName=%newPackageName:Z=z%
-		move .\%oldProjectName%.sublime-project .\%newProjectName%.sublime-project /y >nul 2>nul
-		call uprreplace %newProjectName% %oldProjectName% %newPackageName% %oldPackageName%
-		cd ..
-		move .\%oldProjectName% .\%newProjectName% /y >nul 2>nul
-		cd %newProjectName%
-		goto exit
-	) else (
+	if not exist dev (
 		goto errorDirectory
 	)
+	for %%a in (.) do set oldProjectName=%%~na
+	echo Renaming Project %oldProjectName%
+	set oldPackageName=%oldProjectName%
+	set oldPackageName=%oldPackageName:A=a%
+	set oldPackageName=%oldPackageName:B=b%
+	set oldPackageName=%oldPackageName:C=c%
+	set oldPackageName=%oldPackageName:D=d%
+	set oldPackageName=%oldPackageName:E=e%
+	set oldPackageName=%oldPackageName:F=f%
+	set oldPackageName=%oldPackageName:G=g%
+	set oldPackageName=%oldPackageName:H=h%
+	set oldPackageName=%oldPackageName:I=i%
+	set oldPackageName=%oldPackageName:J=j%
+	set oldPackageName=%oldPackageName:K=k%
+	set oldPackageName=%oldPackageName:L=l%
+	set oldPackageName=%oldPackageName:M=m%
+	set oldPackageName=%oldPackageName:N=n%
+	set oldPackageName=%oldPackageName:O=o%
+	set oldPackageName=%oldPackageName:P=p%
+	set oldPackageName=%oldPackageName:Q=q%
+	set oldPackageName=%oldPackageName:R=r%
+	set oldPackageName=%oldPackageName:S=s%
+	set oldPackageName=%oldPackageName:T=t%
+	set oldPackageName=%oldPackageName:U=u%
+	set oldPackageName=%oldPackageName:V=v%
+	set oldPackageName=%oldPackageName:W=w%
+	set oldPackageName=%oldPackageName:X=x%
+	set oldPackageName=%oldPackageName:Y=y%
+	set oldPackageName=%oldPackageName:Z=z%
+	set newPackageName=%newProjectName%
+	set newPackageName=%newPackageName:A=a%
+	set newPackageName=%newPackageName:B=b%
+	set newPackageName=%newPackageName:C=c%
+	set newPackageName=%newPackageName:D=d%
+	set newPackageName=%newPackageName:E=e%
+	set newPackageName=%newPackageName:F=f%
+	set newPackageName=%newPackageName:G=g%
+	set newPackageName=%newPackageName:H=h%
+	set newPackageName=%newPackageName:I=i%
+	set newPackageName=%newPackageName:J=j%
+	set newPackageName=%newPackageName:K=k%
+	set newPackageName=%newPackageName:L=l%
+	set newPackageName=%newPackageName:M=m%
+	set newPackageName=%newPackageName:N=n%
+	set newPackageName=%newPackageName:O=o%
+	set newPackageName=%newPackageName:P=p%
+	set newPackageName=%newPackageName:Q=q%
+	set newPackageName=%newPackageName:R=r%
+	set newPackageName=%newPackageName:S=s%
+	set newPackageName=%newPackageName:T=t%
+	set newPackageName=%newPackageName:U=u%
+	set newPackageName=%newPackageName:V=v%
+	set newPackageName=%newPackageName:W=w%
+	set newPackageName=%newPackageName:X=x%
+	set newPackageName=%newPackageName:Y=y%
+	set newPackageName=%newPackageName:Z=z%
+	move .\%oldProjectName%.sublime-project .\%newProjectName%.sublime-project >nul 2>nul
+	call uprreplace %newProjectName% %oldProjectName% %newPackageName% %oldPackageName%
+	cd ..
+	move %oldProjectName% .\%newProjectName% >nul 2>nul
+	cd %newProjectName%
+	goto exit
 
 	:exit
--- ssrt.cmd
+++ ssrt.cmd
@@ -14,18 +14,18 @@ set folder=sprites
 	goto options
 
 :sorting
-	set folder2x=%$folder%@2x
-	set folder3x=%$folder%@3x
+	set folder2x=%folder%@2x
+	set folder3x=%folder%@3x
 	md %folder%
 	md %folder2x%
 	md %folder3x%
-	move *@3x.png %folder3x%\
-	move *@3x.jpg %folder3x%\
-	move *@2x.png %folder2x%\
-	move *@2x.jpg %folder2x%\
-	move *.png %folder%\
-	move *.jpg %folder%\
-	move *.svg %folder%\
-	xcopy %folder3x% %folder3x% /S
-	xcopy %folder2x% %folder2x% /S
-	xcopy %folder% %folder% /S
+	move *@3x.png %folder3x%\ >nul 2>nul
+	move *@3x.jpg %folder3x%\ >nul 2>nul
+	move *@2x.png %folder2x%\ >nul 2>nul
+	move *@2x.jpg %folder2x%\ >nul 2>nul
+	move *.png %folder%\ >nul 2>nul
+	move *.jpg %folder%\ >nul 2>nul
+	move *.svg %folder%\ >nul 2>nul
+	xcopy %folder3x% %folder3x% /S /y >nul 2>nul
+	xcopy %folder2x% %folder2x% /S /y >nul 2>nul
+	xcopy %folder% %folder% /S /y >nul 2>nul
