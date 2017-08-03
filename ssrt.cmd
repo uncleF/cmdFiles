9bdcb8fad90eb73500d72dd88f129abbe8e5e080
Ilya Biryukov <uncle.funkay@gmail.com>
2017-08-03 15:54:47 +0300

    Project Update

--- ssrt.cmd
+++ ssrt.cmd
@@ -2,31 +2,31 @@
 
 @echo off
 
-set folder=sprites
+set directory=sprites
 
 :options
 	if [%1]==[] (
 		goto sorting
 	) else {
-		set folder=%1
+		set directory=%1
 	}
 	shift
 	goto options
 
 :sorting
 echo Sorting Sprites
-call usptcopy Phone@3x %folder%
-call usptcopy Phone@2x %folder%
-call usptcopy Phone %folder%
-call usptcopy @3x %folder%
-call usptcopy @2x %folder%
+call usptcopy Phone@3x %directory%
+call usptcopy Phone@2x %directory%
+call usptcopy Phone %directory%
+call usptcopy @3x %directory%
+call usptcopy @2x %directory%
 for %%f in (*@1x.*) do (
 	set filename=%%f
 	set extention=%%~xf
 	ren %filename% %filename:~0,-7%%extention%
 )
-md %folder% >nul 2>nul
-move *.png %folder%\ >nul 2>nul
-move *.jpg %folder%\ >nul 2>nul
-move *.svg %folder%\ >nul 2>nul
-xcopy %folder% %folder% /S /y >nul 2>nul
+md %directory% >nul 2>nul
+move *.png %directory%\ >nul 2>nul
+move *.jpg %directory%\ >nul 2>nul
+move *.svg %directory%\ >nul 2>nul
+xcopy %directory% %directory% /S /y >nul 2>nul
