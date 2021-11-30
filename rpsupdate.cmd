290e99d214224bc6c3a7f1ec33521b499fc142a1
Ilja Biryukov <uncle.funkay@gmail.com>
2021-11-30 19:08:28 +0300

    update

--- rpsupdate.cmd
+++ rpsupdate.cmd
@@ -10,7 +10,11 @@ for /f "delims=" %%d in ('dir /b /ad') do (
 		git fetch --all --prune
 		git pull --all
 		git remote prune origin
-		git remote  update --prune
+		git remote update --prune
+		for /F "tokens=*" %%branch in ('git for-each-ref --format="%%(refname:short)" refs/heads') do (
+			git checkout %%branch
+			git pull
+		)
 		git checkout master
 		git clean -fdx
 		echo ----------------------------------------------------------------------
