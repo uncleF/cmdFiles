fed9b2d428772a6512ef638009352fab4dd1aab2
uncleF <uncleF@users.noreply.github.com>
2025-04-25 20:32:16 +0300

    update

--- rpsupdate.cmd
+++ rpsupdate.cmd
@@ -1,6 +1,7 @@
 :: Update All Repos
 
 @echo off
+setlocal enabledelayedexpansion
 
 for /f "delims=" %%d in ('dir /b /ad') do (
 	cd %%d
@@ -8,23 +9,24 @@ for /f "delims=" %%d in ('dir /b /ad') do (
 		echo %%d 
 		git config core.fileMode false >nul 2>&1
 		git fetch --all --prune >nul 2>&1
-		git pull --all >nul 2>&1
 		git pull --tags >nul 2>&1
-		git remote prune origin >nul 2>&1
-		git remote update --prune >nul 2>&1
 		for /F "tokens=*" %%b in ('git for-each-ref --format="%%(refname:short)" refs/heads') do (
 			for /F "delims=" %%u in ('git for-each-ref --format="%%(upstream:short)" refs/heads/%%b') do (
 				git checkout %%b >nul 2>&1
-				git pull				
+				git pull
 			)
 		)
-		git branch --list | findstr /B /C:"  main" > nul 2>&1
-		if errorlevel 1 (
-			git checkout master >nul 2>&1
-		) else (
-			git checkout main >nul 2>&1
+		set defaultBranch=
+		for /f "tokens=3" %%h in ('git remote show origin ^| findstr "HEAD branch"') do (
+    	set defaultBranch=%%h
 		)
-		git clean -fdx
+		if defined defaultBranch (
+    	git checkout !defaultBranch! >nul 2>&1
+		) else (
+    	echo Failed to detect default branch. Falling back...
+	    git checkout main >nul 2>&1 || git checkout master >nul 2>&1
+		)
+		git clean -fd >nul 2>&1
 		echo ----------------------------------------------------------------------
 	)
 	cd ..
